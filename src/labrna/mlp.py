6709c08d1bdb4dbf47552c6f1939bc4007cca0a2
davips <dpsabc@gmail.com>
2026-09-10 19:30:45 -0300

    mlp example

---
+++ src/labrna/mlp.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+
+class MLP(nn.Module):
+    def __init__(self, n_features):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(n_features, 256), nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 128), nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(128, 1), nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.model(x)
